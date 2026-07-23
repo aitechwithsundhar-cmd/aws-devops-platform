7dc5b25e8b5538f0bb584b3f371c11f78e419775
Sundhar Ajmera <aitechwithsundhar@gmail.com>
2026-07-23 21:27:31 +0700

    refactor(terraform): migrate reusable frontend-alb module

--- terraform/modules/frontend-alb/main.tf
+++ terraform/modules/frontend-alb/main.tf
@@ -1,15 +1,16 @@
 resource "aws_lb" "frontend_alb" {
-  name               = "${var.project}-${var.environment}-frontend"
+  name               = "${var.project}-${var.environment}-frontend-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [local.frontend_alb_sg_id]
   subnets            = local.public_subnet_ids
-  #keep it as false/ just to delete using terrafrom while practice 
+
+  # Keep this false while practicing so Terraform can destroy the ALB.
   enable_deletion_protection = false
 
   tags = merge(
     {
-      Name = "${var.project}-${var.environment}-frontend"
+      Name = "${var.project}-${var.environment}-frontend-alb"
     },
     local.common_tags
   )
@@ -24,24 +25,25 @@ resource "aws_lb_listener" "https" {
 
   default_action {
     type = "fixed-response"
+
     fixed_response {
       content_type = "text/html"
-      message_body = "<h1>hi, iam frrom HTTPS frontend ALB</h1>"
+      message_body = "<h1>Hi, I am from HTTPS Frontend ALB</h1>"
       status_code  = "200"
     }
   }
 }
 
-resource "aws_route53_record" "www" {
+resource "aws_route53_record" "frontend_alb" {
   zone_id = var.zone_id
   name    = "*.${var.domain_name}"
   type    = "A"
 
-  #load_balancer details
+  allow_overwrite = true
+
   alias {
     name                   = aws_lb.frontend_alb.dns_name
     zone_id                = aws_lb.frontend_alb.zone_id
     evaluate_target_health = true
   }
-  allow_overwrite = true
 }
